3236d2a333b4fc8ebab872531de08cb3e74c3139
Edheyberg <edhgama@gmail.com>
2022-04-12 02:23:14 -0400

    inputs

--- scripts.js
+++ scripts.js
@@ -1,8 +1,35 @@
 
 let jogador = null;
+let jogadorSelecionado = document.getElementById('jogador-selecionado')
+
 
 function escolherQuadrado(id){
     
-    console.log('clicou no quadrado '+id)
+    const quadrado = document.getElementById(id);
+    if (quadrado.innerHTML !== '.') 
+        return
+    
+    quadrado.innerHTML = jogador;
+    quadrado.style.backgroundColor = '#faa';
+
+    if (jogador === 'X'){
+        escolheJogador('O')
+    } else {
+        escolheJogador('X')
+    }
 
 }
+
+function escolheJogador(valor){
+
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
+
+
+}
+
+function checaVencedor(){
+    
+}
+
+escolheJogador('X');
